345fb5de8599c1e72543b4bd30ac184271c07082
Kylin Lee <lee.kylin@outlook.com>
2022-09-12 13:29:55 +0800

    fix(image2base64): fix string format
    fix image base64 string format

--- src/image2base64.ts
+++ src/image2base64.ts
@@ -6,34 +6,34 @@ let ext = "png";
 const IMAGE_TYPE_REG = /image\/(j?p[en]?g)/i;
 
 if (!args.length) {
-	throw new Error("image link not found");
+  throw new Error("image link not found");
 }
 
 const imageBuffer = await fetch(link)
-	.then((res) => {
-		if (res.status === 200) {
-			const type = res.headers.get("content-type") ||
-				res.headers.get("ContentType") || "";
-			const regMatch = IMAGE_TYPE_REG.exec(type);
-			if (!regMatch) {
-				return Promise.reject(`not support image type ${type}`);
-			} else {
-				ext = regMatch[1] || ext;
-			}
-			return res.arrayBuffer();
-		}
-		return Promise.reject("fetch image error");
-	})
-	.catch((err) => {
-		console.error(err);
-	});
+  .then((res) => {
+    if (res.status === 200) {
+      const type =
+        res.headers.get("content-type") || res.headers.get("ContentType") || "";
+      const regMatch = IMAGE_TYPE_REG.exec(type);
+      if (!regMatch) {
+        return Promise.reject(`not support image type ${type}`);
+      } else {
+        ext = regMatch[1] || ext;
+      }
+      return res.arrayBuffer();
+    }
+    return Promise.reject("fetch image error");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 if (imageBuffer) {
-	const str = new Uint8Array(imageBuffer).reduce(
-		(acc, next) => acc + String.fromCharCode(next),
-		"",
-	);
-	const base64Str = `data;image/${ext}:base64,${btoa(str)}`;
-	console.info(base64Str);
-	clipboard.writeText(base64Str);
+  const str = new Uint8Array(imageBuffer).reduce(
+    (acc, next) => acc + String.fromCharCode(next),
+    ""
+  );
+  const base64Str = `data:image/${ext};base64,${btoa(str)}`;
+  console.info(base64Str);
+  clipboard.writeText(base64Str);
 }
